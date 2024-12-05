12a332680cf2e4b27074427dd8c38d0c763d3bbc
Md Saiful Islam <mdsaifulislamrafsan099@gmail.com>
2024-12-05 21:53:20 +0600

    added academicSemester controller

---
+++ src/app/modules/academicSemester/academicSemester.controller.ts
@@ -0,0 +1,23 @@
+import { StatusCodes } from "http-status-codes";
+import catchAsync from "../../utils/catchAsync";
+import sendResponse from "../../utils/sendResponse";
+
+
+const createAcademicSemester = catchAsync(async (req , res) => {
+
+    // const result = await 
+
+    sendResponse(res, {
+        statusCode: StatusCodes.OK,
+        success: true,
+         message: 'Academic Semester created successfully',
+         data: result,
+
+    })
+    
+})
+
+
+export const academicSemesterController = {
+    createAcademicSemester
+}
--- src/app/modules/academicSemester/academicSemester.route.ts
+++ src/app/modules/academicSemester/academicSemester.route.ts
@@ -1,7 +1,10 @@
 import express from 'express'
+import { academicSemesterController } from './academicSemester.controller';
+import validateRequest from '../../middleware/validateRequest';
+import { AcademicSemesterValidation } from './AcademicSemester.validation';
 
 const router = express.Router();
 
-router.get('/create-academic-semester', );
+router.get('/create-academic-semester', validateRequest(AcademicSemesterValidation.createAcademicSemesterValidationSchema) , academicSemesterController.createAcademicSemester);
 
 export const AcademicSemesterRoutes = router;
