4d89e1d4ba1eb3a25358c8497ae0113f07278960
Md Saiful Islam <mdsaifulislamrafsan099@gmail.com>
2024-12-07 21:47:38 +0600

    added AcademicFaculty model

---
+++ src/app/modules/academicFaculty/academicFaculty.model.ts
@@ -0,0 +1,17 @@
+import { model, Schema } from "mongoose";
+import { TAcademicFaculty } from "./academicFaculty.interface";
+
+
+const AcademicFacultySchema = new Schema<TAcademicFaculty> ({
+    name : {
+        type : String,
+        required : true,
+        unique : true
+    }
+},{
+    timestamps : true,
+})
+
+const AcademicFaculty = model<TAcademicFaculty>('AcademicFaculty' , AcademicFacultySchema);
+
+export default AcademicFaculty;
