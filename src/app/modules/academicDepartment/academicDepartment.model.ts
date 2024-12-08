6796a9c38969869c258525ae02ee202678bd184c
Md Saiful Islam <mdsaifulislamrafsan099@gmail.com>
2024-12-08 18:44:02 +0600

    added academic department schema model

---
+++ src/app/modules/academicDepartment/academicDepartment.model.ts
@@ -0,0 +1,21 @@
+import { model, Schema } from "mongoose";
+import { TAcademicDepartment } from "./academicDepartment.interface";
+
+const AcademicDepartmentSchema = new Schema<TAcademicDepartment>({
+    name : {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    academicFaculty : {
+        type: Schema.Types.ObjectId,
+        ref: 'AcademicFaculty',
+    }
+},
+    {
+        timestamps: true
+    }
+);
+
+const AcademicDepartment =  model<TAcademicDepartment>("AcademicDepartment" , AcademicDepartmentSchema);
+export default AcademicDepartment;
