442ba871b3d7bfcd130f54dc006b71001cd6f40d
Md Saiful Islam <mdsaifulislamrafsan099@gmail.com>
2024-12-15 19:40:42 +0600

    added unhandledRejection error handleing

--- src/server.ts
+++ src/server.ts
@@ -1,22 +1,31 @@
 import app from './app';
 import config from './app/config';
 import mongoose from 'mongoose';
-
-
+import {Server} from "http"
+let server : Server;
 
 const connectDB = async () => {
   try {
     await mongoose.connect(config.db_url as string);
     console.log('Database connected');
 
-    app.listen(config.port, () => {
+    server = app.listen(config.port, () => {
       console.log(`Example app listening on port ${config.port}`);
     });
   } catch (error) {
     console.error('Database connection failed:', error);
-    process.exit(1);
   }
 };
 connectDB();
+
+process.on('unhandledRejection' , ()=>{
+    console.log('🚨 Unhandled rejection, exiting now...');
+    if(server){
+        server.close(()=>{
+            process.exit(1);
+        })
+    }
+    process.exit(1);
+})
 
 
