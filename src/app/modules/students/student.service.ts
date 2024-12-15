65c2fe1ebd6d3031879aafaca0ae22324d44c827
Md Saiful Islam <mdsaifulislamrafsan099@gmail.com>
2024-12-15 21:35:04 +0600

    added student search funcationality base email or firstName or presentAddress

--- src/app/modules/students/student.service.ts
+++ src/app/modules/students/student.service.ts
@@ -5,8 +5,19 @@ import { StatusCodes } from 'http-status-codes';
 import { TStudent } from './student.interface';
 import User from '../user/user.model';
 
-const getAllStudentsFromDB = async () => {
-  const result = await Student.find()
+const getAllStudentsFromDB = async (query: Record<string, unknown>) => {
+  console.log('base query', query)
+  let searchTerm = '';
+
+  if (query?.searchTerm) {
+    searchTerm = query?.searchTerm as string;
+  }
+const studentSearchAbelQuery = ['email', 'name.firstName', 'presentAddress']
+  const result = await Student.find({
+    $or: studentSearchAbelQuery.map((field) => ({
+      [field]: { $regex: searchTerm, $options : 'i' },
+    })),
+  })
     .populate('admissionSemester')
     .populate({
       path: 'academicDepartment',
@@ -16,90 +27,83 @@ const getAllStudentsFromDB = async () => {
 };
 
 const getSingleStudentFromDB = async (id: string) => {
-  
-  const result = await Student.findOne({id})
+  const result = await Student.findOne({ id })
     .populate('admissionSemester')
     .populate({ path: 'academicDepartment', populate: 'academicFaculty' });
   return result;
 };
 
-const updatedStudentFromDB = async ( id : string , payload : Partial<TStudent>) => {
+const updatedStudentFromDB = async (id: string, payload: Partial<TStudent>) => {
+  const { name, localGuardian, guardian, ...remainingStudentData } = payload;
 
-  const {name , localGuardian , guardian, ...remainingStudentData} = payload;
-
-  const modifiedUpdatedData : Record<string , unknown> =  {
-    ...remainingStudentData
-  }
-  if(name && Object.keys(name).length){
-    for(const [key , value] of Object.entries(name)){
-      modifiedUpdatedData[`name.${key}`] = value
+  const modifiedUpdatedData: Record<string, unknown> = {
+    ...remainingStudentData,
+  };
+  if (name && Object.keys(name).length) {
+    for (const [key, value] of Object.entries(name)) {
+      modifiedUpdatedData[`name.${key}`] = value;
     }
   }
 
-  if(localGuardian && Object.keys(localGuardian).length){
-    for(const [key , value] of Object.entries(localGuardian)){
-      modifiedUpdatedData[`localGuardian.${key}`] = value
+  if (localGuardian && Object.keys(localGuardian).length) {
+    for (const [key, value] of Object.entries(localGuardian)) {
+      modifiedUpdatedData[`localGuardian.${key}`] = value;
     }
   }
 
-  if(guardian && Object.keys(guardian).length){
-    for(const [key, value] of Object.entries(guardian)){
+  if (guardian && Object.keys(guardian).length) {
+    for (const [key, value] of Object.entries(guardian)) {
       modifiedUpdatedData[`guardian.${key}`] = value;
     }
   }
 
-
-  const result = await Student.findOneAndUpdate({id} , modifiedUpdatedData , {new : true} );
+  const result = await Student.findOneAndUpdate({ id }, modifiedUpdatedData, {
+    new: true,
+  });
   return result;
-
-}
-
+};
 
 const deleteStudentFromDB = async (id: string) => {
-  // create session 
+  // create session
 
   const session = await mongoose.startSession();
 
-  
+  try {
+    // start Transaction
+    session.startTransaction();
 
- try {
-  // start Transaction
-  session.startTransaction();
+    // Transaction - 1
+    const deletedStudent = await Student.findOneAndUpdate(
+      { id },
+      { isDeleted: true },
+      { new: true },
+    );
+    if (!deletedStudent) {
+      throw new AppError(StatusCodes.NOT_FOUND, 'Student not found');
+    }
+    // Transaction - 2
+    const deletedUser = await User.findOneAndUpdate(
+      { id },
+      { isDeleted: true },
+      { new: true },
+    );
+    if (!deletedUser) {
+      throw new AppError(StatusCodes.NOT_FOUND, 'User not found');
+    }
 
-  // Transaction - 1
-  const deletedStudent = await Student.findOneAndUpdate(
-    { id},
-    { isDeleted: true },
-    {new : true}
-  );
-  if (!deletedStudent) {
-    throw new AppError(StatusCodes.NOT_FOUND, 'Student not found');
+    await session.commitTransaction();
+    await session.endSession();
+    return deletedStudent;
+  } catch (error) {
+    await session.abortTransaction();
+    await session.endSession();
+    throw new AppError(StatusCodes.BAD_REQUEST, 'Failed to Delete');
   }
-// Transaction - 2
-  const deletedUser = await User.findOneAndUpdate(
-    {id},
-    {isDeleted : true},
-    {new: true}
-  )
-  if (!deletedUser) {
-    throw new AppError(StatusCodes.NOT_FOUND, 'User not found');
-  }
-
-
-  await session.commitTransaction();
-  await session.endSession();
-  return deletedStudent;
- } catch (error) {
-  await session.abortTransaction();
-  await session.endSession();
-  throw new AppError(StatusCodes.BAD_REQUEST, 'Failed to Delete' )
-
- }
 };
 
 export const StudentServices = {
   getAllStudentsFromDB,
   getSingleStudentFromDB,
   deleteStudentFromDB,
-  updatedStudentFromDB
+  updatedStudentFromDB,
 };
