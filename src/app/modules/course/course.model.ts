09b4a73224cd9089fc327e8ea501ee0ac1b0b8c6
Md Saiful Islam <mdsaifulislamrafsan099@gmail.com>
2024-12-17 03:00:12 +0600

    added course model schema

--- src/app/modules/course/course.model.ts
+++ src/app/modules/course/course.model.ts
@@ -1,13 +1,42 @@
-import { Schema, Types } from "mongoose";
-import { TPreRequisiteCourse } from "./course.interface";
+import { model, Schema } from 'mongoose';
+import { TCourse, TPreRequisiteCourse } from './course.interface';
 
 const preRequisiteCourseSchema = new Schema<TPreRequisiteCourse>({
-    course: {
-        type: Schema.Types.ObjectId,
-    },
-    isDeleted: {
-        type: Boolean,
-        default: false
-    }
+  course: {
+    type: Schema.Types.ObjectId,
+  },
+  isDeleted: {
+    type: Boolean,
+    default: false,
+  },
+});
 
-})
+
+
+const courseSchema = new Schema<TCourse>({
+  title: {
+    type: String,
+    required: true,
+    trim: true,
+    unique: true,
+  },
+  prefix: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  code: {
+    type: Number,
+    required: true,
+    trim: true,
+  },
+  credits: {
+    type: Number,
+    required: true,
+    trim: true,
+  },
+  preRequisiteCourse: [preRequisiteCourseSchema],
+});
+
+const Course = model<TCourse>('Course', courseSchema)
+export default Course;
