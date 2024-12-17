ef394bea5fabd24c0360085f5add0a5ced8a5454
Md Saiful Islam <mdsaifulislamrafsan099@gmail.com>
2024-12-17 18:06:55 +0600

    added create course validation using zod

--- src/app/modules/course/course.route.ts
+++ src/app/modules/course/course.route.ts
@@ -1,8 +1,10 @@
 import express from 'express';
 import { courseController } from './course.controller';
+import validateRequest from '../../middleware/validateRequest';
+import { courseValidation } from './course.validation';
 const router = express.Router();
 
-router.post('/', courseController.createCourse);
+router.post('/', validateRequest(courseValidation.createCourseValidation) , courseController.createCourse);
 router.get('/', courseController.getAllCourses);
 router.get('/:id', courseController.getSingleCourse);
 router.delete('/:id', courseController.deleteCourse)
