7bb62f6ccf13e5f2ca84d51c380332fb989989bb
Md Saiful Islam <mdsaifulislamrafsan099@gmail.com>
2024-12-18 12:28:33 +0600

    added Transaction and role back use for course

--- src/app/modules/course/course.service.ts
+++ src/app/modules/course/course.service.ts
@@ -2,6 +2,9 @@ import { TCourse } from './course.interface';
 import Course from './course.model';
 import QueryBuilder from './../../builder/QueryBuilder';
 import { CourseSearchableFields } from './course.constant';
+import mongoose from 'mongoose';
+import AppError from '../../errors/AppErrors';
+import { StatusCodes } from 'http-status-codes';
 
 const createCourseIntoDB = async (payload: TCourse) => {
   const result = await Course.create(payload);
@@ -40,53 +43,91 @@ const deleteCourseFromDB = async (id: string) => {
 const updateCourseFromDB = async (id: string, payload: Partial<TCourse>) => {
   const { preRequisiteCourse, ...courseRemainingData } = payload;
 
-  // step-1: basic course info update
+  const session = await mongoose.startSession();
 
-  const updateBasicCourseInfo = await Course.findByIdAndUpdate(
-    id,
-    courseRemainingData,
-    {
-      new: true,
-      runValidators: true,
-    },
-  );
+  try {
+    session.startTransaction();
 
-  // step-2: delete preRequisite course
-  if (preRequisiteCourse && preRequisiteCourse.length > 0) {
-    const deletedPreRequisite = preRequisiteCourse
-      .filter((item) => item?.course && item?.isDeleted)
-      .map((el) => el.course);
+    // step-1: basic course info update
 
-    const deletedPreRequisiteCourses = await Course.findByIdAndUpdate(
+    const updateBasicCourseInfo = await Course.findByIdAndUpdate(
       id,
+      courseRemainingData,
       {
-        $pull: { preRequisiteCourse: { course: { $in: deletedPreRequisite } } },
-      },
-      {
+        session,
         new: true,
+        runValidators: true,
       },
     );
+    if (!updateBasicCourseInfo) {
+      throw new AppError(
+        StatusCodes.INTERNAL_SERVER_ERROR,
+        'Failed to update course.',
+      );
+    }
 
-    // step-3: update new preRequisite course
-    const newPreRequisite = preRequisiteCourse
-      .filter((item) => item.course && !item.isDeleted);
+    // step-2: delete preRequisite course
+    if (preRequisiteCourse && preRequisiteCourse.length > 0) {
+      const deletedPreRequisite = preRequisiteCourse
+        .filter((item) => item?.course && item?.isDeleted)
+        .map((el) => el.course);
 
+      const deletedPreRequisiteCourses = await Course.findByIdAndUpdate(
+        id,
+        {
+          $pull: {
+            preRequisiteCourse: { course: { $in: deletedPreRequisite } },
+          },
+        },
+        { session, validateRequest: true, new: true },
+      );
 
-    const newPreRequisiteCourse = await Course.findByIdAndUpdate(
-      id,
-      {
-        $addToSet: { preRequisiteCourse: { $each: newPreRequisite } },
-      },
-      {
-        new: true,
-        runValidators: true
-      },
+      if (!deletedPreRequisiteCourses) {
+        throw new AppError(
+          StatusCodes.INTERNAL_SERVER_ERROR,
+          'Failed to delete preRequisite course.',
+        );
+      }
+
+      // step-3: update new preRequisite course
+      const newPreRequisite = preRequisiteCourse.filter(
+        (item) => item.course && !item.isDeleted,
+      );
+
+      const newPreRequisiteCourse = await Course.findByIdAndUpdate(
+        id,
+        {
+          $addToSet: { preRequisiteCourse: { $each: newPreRequisite } },
+        },
+        {
+          new: true,
+          runValidators: true,
+          session,
+        },
+      );
+      if (!newPreRequisiteCourse) {
+        throw new AppError(
+          StatusCodes.INTERNAL_SERVER_ERROR,
+          'Failed to update preRequisite course.',
+        );
+      }
+      const result = await Course.findById(id).populate(
+        'preRequisiteCourse.course',
+      );
+
+      session.commitTransaction();
+      session.endSession();
+      return result;
+    }
+  } catch (error) {
+    session.abortTransaction();
+    session.endSession();
+
+    throw new AppError(
+      StatusCodes.BAD_REQUEST,
+      'Course not found or not found in the database',
     );
   }
-
-  const result =  await Course.findById(id).populate('preRequisiteCourse.course')
-
-  return result;
 };
 
 export const courseService = {
