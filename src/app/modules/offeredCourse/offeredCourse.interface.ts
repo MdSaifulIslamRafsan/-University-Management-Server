3fb43dab332b1e19b7f944f9147326f0ce6716c0
Md Saiful Islam <mdsaifulislamrafsan099@gmail.com>
2024-12-20 00:51:36 +0600

    added Days type

--- src/app/modules/offeredCourse/offeredCourse.interface.ts
+++ src/app/modules/offeredCourse/offeredCourse.interface.ts
@@ -1,15 +1,25 @@
-import { Types } from "mongoose";
+import { Types } from 'mongoose';
+
+export type Days = 
+  | "sat"
+  | "sun"
+  | "mon"
+  | "tue"
+  | "wed"
+  | "thu"
+  | "fri";
+
 
 export interface TOfferedCourse {
-    semesterRegistration : Types.ObjectId;
-    academicSemester : Types.ObjectId;
-    academicFaculty : Types.ObjectId;
-    academicDepartment : Types.ObjectId;
-    course : Types.ObjectId;
-    faculty : Types.ObjectId;
-    maxCapacity :number;
-    section : number;
-    
-    startTime : string;
-    endTime : string;
+  semesterRegistration: Types.ObjectId;
+  academicSemester: Types.ObjectId;
+  academicFaculty: Types.ObjectId;
+  academicDepartment: Types.ObjectId;
+  course: Types.ObjectId;
+  faculty: Types.ObjectId;
+  maxCapacity: number;
+  section: number;
+  days: Days;
+  startTime: string;
+  endTime: string;
 }
