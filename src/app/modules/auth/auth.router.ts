801e3bf0409fc7ef3bc09e9345842bd3fdbe1847
Md Saiful Islam <mdsaifulislamrafsan099@gmail.com>
2024-12-21 02:41:10 +0600

    added login user service and controller

--- src/app/modules/auth/auth.router.ts
+++ src/app/modules/auth/auth.router.ts
@@ -3,8 +3,9 @@
 import express from 'express';
 import validateRequest from '../../middleware/validateRequest';
 import { AuthValidation } from './auth.validation';
+import { AuthController } from './auth.controller';
 const router = express.Router();
 
-router.post('/login', validateRequest(AuthValidation.loginUserValidation), )
+router.post('/login', validateRequest(AuthValidation.loginUserValidation), AuthController.loginUser )
 
 export const AuthRoutes =  router;
---
+++ src/app/modules/auth/auth.service.ts
@@ -0,0 +1,38 @@
+import { StatusCodes } from "http-status-codes";
+import AppError from "../../errors/AppErrors";
+import User from "../user/user.model";
+import { TLoginUser } from "./auth.interface";
+import bcrypt from "bcrypt"
+const loginUserFromDB = async(payload : TLoginUser)=>{
+
+    const {id , password} = payload;
+
+    const isUserExist = await User.findOne({id});
+
+    if(!isUserExist) {
+        throw new AppError(StatusCodes.NOT_FOUND, 'User not found')
+    }
+    const isDeleted = isUserExist?.isDeleted
+    if(isDeleted) {
+        throw new AppError(StatusCodes.NOT_FOUND, 'User not found')
+    }
+
+    const userStatus = isUserExist?.status;
+    if(userStatus === 'blocked') {
+        throw new AppError(StatusCodes.FORBIDDEN, 'User is blocked')
+    }
+
+    const isValidPassword = bcrypt.compareSync(password, isUserExist.password);
+
+    if(!isValidPassword){
+        throw new AppError(StatusCodes.FORBIDDEN, 'Invalid password')
+    }
+
+
+
+    return {};
+}
+
+export const AuthService = {
+    loginUserFromDB
+}
